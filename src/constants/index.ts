81163c92ea2d0fb42cf7e247371aa390be496a17
davidpinkevich <davidpinkevich95@gmail.com>
2024-06-18 18:46:23 +0300

    feat: add links constants

---
+++ src/constants/index.ts
@@ -0,0 +1,8 @@
+const LINKS_HEADER = [
+  { name: "Home", path: "/" },
+  { name: "Timeline", path: "timeline" },
+  { name: "Bank card", path: "card" },
+  { name: "Contacts", path: "contacts" }
+];
+
+export { LINKS_HEADER };
