e7873ad5f63b768a0820ee4a3cf3042a7fce2c2c
davidpinkevich <davidpinkevich95@gmail.com>
2024-06-19 18:38:46 +0300

    feat: add theme for poster info

--- src/components/Poster/PosterInfo/index.tsx
+++ src/components/Poster/PosterInfo/index.tsx
@@ -1,10 +1,24 @@
+import classNames from "classnames";
+
+import { useTheme } from "@hooks/useTheme";
+
 import styles from "./styles.module.scss";
 
 const PosterInfo: React.FC = () => {
+  const theme = useTheme();
+
   return (
     <div className={styles.poster_info}>
       <h1 className={styles.poster_info_title}>Modsen Currency Tracker</h1>
-      <p className={styles.poster_info_describe}>
+      <p
+        className={
+          theme === "dark"
+            ? styles.poster_info_describe
+            : classNames(
+                styles.poster_info_describe,
+                styles.poster_info_describe_white
+              )
+        }>
         Quotes for the dollar and other international currencies.
       </p>
     </div>
