395d1e1826d396dbf49a20a4e4990e5db92f059d
davidpinkevich <davidpinkevich95@gmail.com>
2024-06-19 15:11:31 +0300

    feat: create store with persist

--- src/redux/store/index.ts
+++ src/redux/store/index.ts
@@ -1,10 +1,43 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+
+import sliceMemory from "../slices/sliceMemory";
+import sliceTracker from "../slices/sliceTracker";
+
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  persistReducer,
+  persistStore,
+  PURGE,
+  REGISTER,
+  REHYDRATE
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const rootReducer = combineReducers({ sliceTracker, sliceMemory });
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["sliceMemory"]
+};
+
+const persidtedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
-  reducer: {}
+  reducer: persidtedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+      }
+    })
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+
+export const persistor = persistStore(store);
 
 export { store };
