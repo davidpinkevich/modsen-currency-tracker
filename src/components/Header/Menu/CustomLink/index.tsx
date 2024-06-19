ab789b88a04f68a22fb9d3280f92909c69dadc80
davidpinkevich <davidpinkevich95@gmail.com>
2024-06-19 18:39:19 +0300

    feat: add theme for custom link

--- src/components/Header/Menu/CustomLink/index.tsx
+++ src/components/Header/Menu/CustomLink/index.tsx
@@ -1,12 +1,22 @@
 import { Link } from "react-router-dom";
+import classNames from "classnames";
 
+import { useTheme } from "@hooks/useTheme";
 import { type TypeLinkHeader } from "@src/types";
 
 import styles from "./styles.module.scss";
 
 const CustomLink: React.FC<TypeLinkHeader> = ({ name, path }) => {
+  const theme = useTheme();
+
   return (
-    <Link className={styles.link} to={path}>
+    <Link
+      className={
+        theme === "dark"
+          ? styles.link
+          : classNames(styles.link, styles.link_white)
+      }
+      to={path}>
       {name}
     </Link>
   );
