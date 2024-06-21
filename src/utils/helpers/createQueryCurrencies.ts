207d5be9d8e86bddb756f9e438163936b56356bb
davidpinkevich <davidpinkevich95@gmail.com>
2024-06-21 15:26:42 +0300

    feat: add create query

--- src/utils/helpers/createQueryCurrencies.ts
+++ src/utils/helpers/createQueryCurrencies.ts
@@ -1,4 +1,4 @@
-const createQueryCurrencies = (arr: Array<string>): string => {
+const createQueryCurrencies = (arr: string[]): string => {
   return (
     `${process.env.URL_CURRENCIES}?apikey=${process.env.APIKEY_CURRENCIES}&currencies=` +
     arr.join("%2C")
