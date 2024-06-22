30e4877e039dcb72d86b7e722372f4bfb5f49ab6
davidpinkevich <davidpinkevich95@gmail.com>
2024-06-23 00:20:59 +0300

    feat: add hook conversion

---
+++ src/hooks/useConversion.ts
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+
+import { useAppDispatch, useAppSelector } from "@hooks/useRedux";
+import { useTheme } from "@hooks/useTheme";
+import { getDataCurrencies } from "@redux/slices/sliceMemory";
+import { getConversionISO } from "@redux/slices/sliceTracker";
+
+const useConversion = () => {
+  const theme = useTheme();
+  const data = useAppSelector(getDataCurrencies);
+  const conversionISO = useAppSelector(getConversionISO);
+  const dispatch = useAppDispatch();
+
+  const [input, setInput] = useState("");
+  const [result, setResult] = useState("");
+
+  useEffect(() => {
+    function convertCurrency(from: string, to: string, amount: string): string {
+      if (from === to) {
+        return amount;
+      }
+      if (data && from && to && input.length) {
+        const fromValue = data[from].value;
+        const toValue = data[to].value;
+
+        const result = (Number(amount) / fromValue) * toValue;
+
+        return result.toFixed(6);
+      }
+      return "";
+    }
+    setResult(convertCurrency(conversionISO.from, conversionISO.to, input));
+  }, [input, conversionISO.to]);
+
+  return { input, setInput, result, dispatch, conversionISO, theme };
+};
+
+export { useConversion };
