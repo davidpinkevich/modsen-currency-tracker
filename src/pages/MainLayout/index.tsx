c276d689cda71360e61e9140bfb3516f472fe46e
davidpinkevich <davidpinkevich95@gmail.com>
2024-06-22 13:12:26 +0300

    feat: add timestamp

--- src/pages/MainLayout/index.tsx
+++ src/pages/MainLayout/index.tsx
@@ -1,16 +1,23 @@
 import { Outlet } from "react-router-dom";
 
+import { createTimeUpdate } from "@utils/helpers/createTimeUpdate";
+import { useFetchCurrencies } from "@hooks/useFetchCurrencies";
+
 import { Footer } from "@components/Footer";
 import { Header } from "@components/Header";
 import { Poster } from "@components/Poster";
+import { TimeStamp } from "@components/TimeStamp";
 
 import styles from "./styles.module.scss";
 
 const MainLayout: React.FC = () => {
+  const { timeStamp } = useFetchCurrencies(20000);
+
   return (
     <div className={styles.layout}>
       <Header />
       <Poster />
+      <TimeStamp timeStamp={timeStamp} />
       <main>
         <Outlet />
       </main>
