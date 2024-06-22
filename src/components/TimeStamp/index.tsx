d3f2b90ac066d19d907cea803fcf95f4258cb43f
davidpinkevich <davidpinkevich95@gmail.com>
2024-06-22 13:12:26 +0300

    feat: add timestamp

---
+++ src/components/TimeStamp/index.tsx
@@ -0,0 +1,26 @@
+import classNames from "classnames";
+
+import { ThemeMode } from "@constants/themeMode";
+import { createTimeUpdate } from "@utils/helpers/createTimeUpdate";
+import { useTheme } from "@hooks/useTheme";
+
+import styles from "./styles.module.scss";
+
+const TimeStamp: React.FC<{ timeStamp: number }> = ({ timeStamp }) => {
+  const theme = useTheme();
+
+  return (
+    <div
+      className={
+        theme === ThemeMode.dark
+          ? styles.timestamp
+          : classNames(styles.timestamp, styles.timestamp_white)
+      }>
+      <span />
+      <span />
+      Last updated at {createTimeUpdate(timeStamp)}
+    </div>
+  );
+};
+
+export { TimeStamp };
