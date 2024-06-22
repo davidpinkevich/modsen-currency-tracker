3f885605065a80753e5ec33fd9faa6b40bfcb75d
davidpinkevich <davidpinkevich95@gmail.com>
2024-06-22 13:30:55 +0300

    refactor: delete type mock stoks value

--- src/types/CurrencyItems.types.ts
+++ src/types/CurrencyItems.types.ts
@@ -1,4 +1,4 @@
 export interface TypeCurrencyItems {
-  STOKS: Array<{ title: string; value: string; img: string }>;
+  STOKS: Array<{ title: string; img: string }>;
   QUOTES: Array<{ title: string; iso: string; img: string }>;
 }
