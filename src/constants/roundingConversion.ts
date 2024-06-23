af14cc9ab4f1ccb1799883607df81e81be93087a
davidpinkevich <davidpinkevich95@gmail.com>
2024-06-23 14:01:00 +0300

    refactor: create constants for view conversion

---
+++ src/constants/roundingConversion.ts
@@ -0,0 +1,11 @@
+import { type TypeRoundingConversion } from "@src/types";
+
+const ROUNDING_CONVERSION: TypeRoundingConversion = {
+  REAL_RATE: 3,
+  MIN_VALUE: 1,
+  SLICE_MIN: 2,
+  SLICE_MAX: 6,
+  VIEW_RESULT: 9
+};
+
+export { ROUNDING_CONVERSION };
--- src/utils/helpers/getConversion.ts
+++ src/utils/helpers/getConversion.ts
@@ -1,6 +1,8 @@
+import { ROUNDING_CONVERSION } from "@constants/index";
+
 const getConversion = (value: string) => {
   const res = value.replace(/[^0-9]/g, "");
-  return res.slice(0, 9);
+  return res.slice(0, ROUNDING_CONVERSION.VIEW_RESULT);
 };
 
 const sliceConversion = (
@@ -10,7 +12,9 @@ const sliceConversion = (
 ): string => {
   const result = (Number(amount) / fromValue) * toValue;
 
-  return result > 1 ? result.toFixed(2) : result.toFixed(6);
+  return result > ROUNDING_CONVERSION.MIN_VALUE
+    ? result.toFixed(ROUNDING_CONVERSION.SLICE_MIN)
+    : result.toFixed(ROUNDING_CONVERSION.SLICE_MAX);
 };
 
 export { getConversion, sliceConversion };
