cc334831ba9fc51a8e6a4029f844ebbbebebd73c
davidpinkevich <davidpinkevich95@gmail.com>
2024-06-23 14:01:00 +0300

    refactor: create constants for view conversion

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,5 @@
 import { LINKS_HEADER } from "./linksHeader";
 import { NAMES_CURRENCY } from "./NamesCurrency";
+import { ROUNDING_CONVERSION } from "./roundingConversion";
 
-export { LINKS_HEADER, NAMES_CURRENCY };
+export { LINKS_HEADER, NAMES_CURRENCY, ROUNDING_CONVERSION };
--- src/types/index.ts
+++ src/types/index.ts
@@ -2,10 +2,12 @@ import { type CurrencyItem } from "./CurrencyItem.types";
 import { type TypeCurrencyItems } from "./CurrencyItems.types";
 import { type TypeDataCurrency } from "./DataCurrency.types";
 import { type TypeLinkHeader } from "./LinkHeader.types";
+import { type TypeRoundingConversion } from "./RoundingConversion.types";
 
 export type {
   CurrencyItem,
   TypeCurrencyItems,
   TypeDataCurrency,
-  TypeLinkHeader
+  TypeLinkHeader,
+  TypeRoundingConversion
 };
