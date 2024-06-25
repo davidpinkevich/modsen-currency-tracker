c4f5a593fdc5ebb9ed094b362273244644d185ad
davidpinkevich <davidpinkevich95@gmail.com>
2024-06-25 18:37:57 +0300

    feat: add types for data timeseries

---
+++ src/types/DataTimeseries.types.ts
@@ -0,0 +1,12 @@
+export interface TypeItemTimeseries {
+  rate_close: number;
+  rate_high: number;
+  rate_low: number;
+  rate_open: number;
+  time_close: string;
+  time_open: string;
+  time_period_end: string;
+  time_period_start: string;
+}
+
+export type TypeDataTimeseries = Array<Record<string, TypeItemTimeseries>>;
