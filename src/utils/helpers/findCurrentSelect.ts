42f2a38a6fa24f69c3ee3f018f2e585f9e8ff33c
davidpinkevich <davidpinkevich95@gmail.com>
2024-06-28 15:18:23 +0300

    fix: change path for constant

--- src/utils/helpers/findCurrentSelect.ts
+++ src/utils/helpers/findCurrentSelect.ts
@@ -1,10 +1,10 @@
 import { type TypeCurrencyItems } from "@src/types";
 
 const findCurrentSelect = (
-  data: TypeCurrencyItems,
+  data: Array<{ title: string; iso: string }>,
   value: string
 ): string | undefined => {
-  return data.QUOTES.find((item) => item.iso === value)?.title;
+  return data.find((item) => item.iso === value)?.title;
 };
 
 export { findCurrentSelect };
