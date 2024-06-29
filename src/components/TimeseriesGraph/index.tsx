fc6e7664ff98e4056d591b396166df600e8b769d
davidpinkevich <davidpinkevich95@gmail.com>
2024-06-29 23:01:18 +0300

    feat: add view theme

--- src/components/TimeseriesGraph/index.tsx
+++ src/components/TimeseriesGraph/index.tsx
@@ -1,7 +1,11 @@
 import { Component } from "react";
 import { Bar } from "react-chartjs-2";
+import { connect } from "react-redux";
+import classNames from "classnames";
 
+import { ThemeMode } from "@constants/themeMode";
 import { createGraphConfig } from "@utils/helpers/createGraphConfig";
+import { type RootState } from "@redux/store";
 import { type PropsTimeseriesGraph } from "@src/types";
 
 import "chartjs-adapter-date-fns";
@@ -31,17 +35,30 @@ ChartJS.register(
 
 class TimeseriesGraph extends Component<PropsTimeseriesGraph> {
   render() {
-    const { dataTimeseries } = this.props;
+    const { dataTimeseries, theme } = this.props;
 
     if (dataTimeseries?.length) {
       const { data, options, plugins } = createGraphConfig(dataTimeseries);
       return (
-        <div className={styles.graph}>
-          <Bar options={options} data={data} plugins={plugins} />
+        <div
+          className={
+            theme === ThemeMode.DARK
+              ? styles.graph
+              : classNames(styles.graph, styles.graph_white)
+          }>
+          {dataTimeseries.length >= 30 ? (
+            <Bar options={options} data={data} plugins={plugins} />
+          ) : (
+            <p>Please indicate a period of 30 days</p>
+          )}
         </div>
       );
     }
   }
 }
 
-export { TimeseriesGraph };
+const mapStateToProps = (state: RootState) => ({
+  theme: state.sliceMemory.theme
+});
+
+export default connect(mapStateToProps)(TimeseriesGraph);
