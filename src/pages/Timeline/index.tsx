3e1983a3b21a32918b1df2c56a6b6416e3b7a02c
davidpinkevich <davidpinkevich95@gmail.com>
2024-06-29 23:02:02 +0300

    feat: add timeline

---
+++ src/pages/Timeline/index.tsx
@@ -0,0 +1,59 @@
+import { Component } from "react";
+import { connect, shallowEqual } from "react-redux";
+
+import { fetchTimeseries } from "@redux/slices/sliceTracker";
+import { type AppDispatch, type RootState } from "@redux/store";
+import { type PropsTimeline } from "@src/types";
+
+import { Loading } from "@components/Loading";
+import TimeseriesFilters from "@components/TimeseriesFilters";
+import TimeseriesGraph from "@components/TimeseriesGraph";
+
+import styles from "./styles.module.scss";
+
+class Timeline extends Component<PropsTimeline> {
+  componentDidMount() {
+    const { paramsTimeseries, fetchTimeseries } = this.props;
+    fetchTimeseries(paramsTimeseries);
+  }
+
+  componentDidUpdate(prevProps: PropsTimeline) {
+    const { paramsTimeseries, fetchTimeseries } = this.props;
+
+    if (!shallowEqual(prevProps.paramsTimeseries, paramsTimeseries)) {
+      fetchTimeseries(paramsTimeseries);
+    }
+  }
+
+  render() {
+    const { dataTimeseries, loading } = this.props;
+
+    return (
+      <div className={styles.timeline}>
+        <TimeseriesFilters />
+        {loading ? (
+          <Loading value="Loading" />
+        ) : (
+          <TimeseriesGraph dataTimeseries={dataTimeseries} />
+        )}
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state: RootState) => ({
+  dataTimeseries: state.sliceTracker.dataTimeseries,
+  paramsTimeseries: state.sliceTracker.paramsTimeseries,
+  loading: state.sliceTracker.loading
+});
+
+const mapDispatchToProps = (dispatch: AppDispatch) => ({
+  fetchTimeseries: async (params: {
+    from: string;
+    to: string;
+    start: string;
+    end: string;
+  }) => await dispatch(fetchTimeseries(params))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Timeline);
