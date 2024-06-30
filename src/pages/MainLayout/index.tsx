5031f96a8e77d6a3d1de5f8e6584c1f7ba38e780
davidpinkevich <davidpinkevich95@gmail.com>
2024-06-30 13:48:17 +0300

    feat: add error boundary

--- src/pages/MainLayout/index.tsx
+++ src/pages/MainLayout/index.tsx
@@ -3,6 +3,7 @@ import { Outlet } from "react-router-dom";
 import { TIMESTAMP } from "@constants/index";
 import { useFetchCurrencies } from "@hooks/useFetchCurrencies";
 
+import { ErrorBoundary } from "@components/ErrorBoundary";
 import { Footer } from "@components/Footer";
 import { Header } from "@components/Header";
 import { Poster } from "@components/Poster";
@@ -15,13 +16,19 @@ const MainLayout: React.FC = () => {
 
   return (
     <div className={styles.layout}>
-      <Header />
-      <Poster />
-      <TimeStamp timeStamp={timeStamp} />
-      <main>
-        <Outlet />
-      </main>
-      <Footer />
+      <ErrorBoundary>
+        <Header />
+        <Poster />
+        <TimeStamp timeStamp={timeStamp} />
+      </ErrorBoundary>
+      <ErrorBoundary>
+        <main>
+          <Outlet />
+        </main>
+      </ErrorBoundary>
+      <ErrorBoundary>
+        <Footer />
+      </ErrorBoundary>
     </div>
   );
 };
