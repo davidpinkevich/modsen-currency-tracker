3ff6ff82c78a77a2d4dfcea811b23523b5e4e83b
davidpinkevich <davidpinkevich95@gmail.com>
2024-07-03 14:20:47 +0300

    fix: delete block

--- src/pages/BankCard/index.tsx
+++ src/pages/BankCard/index.tsx
@@ -2,15 +2,12 @@ import { Component } from "react";
 
 import { BankMap } from "@components/BankMap";
 
-import styles from "./styles.module.scss";
-
 class BankCard extends Component {
   render() {
     return (
-      <div>
-        <h2>BankCard</h2>
+      <>
         <BankMap />
-      </div>
+      </>
     );
   }
 }
